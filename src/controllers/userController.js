d4f6b5c4af34cfb432a445c53016134b409e8910
Aarya Patel <aaryapatel9900@gmail.com>
2025-11-18 13:12:30 -0500

    Update user controller logic

---
+++ src/controllers/userController.js
@@ -0,0 +1,42 @@
+import {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+  } from '../services/userService.js';
+  
+
+  export async function getAllUsersHandler(req, res) {
+    const users = await getAllUsers();
+    res.status(200).json(users);
+  }
+  
+
+  export async function getUserByIdHandler(req, res) {
+    const id = parseInt(req.params.id);
+    const user = await getUserById(id);
+    res.status(200).json(user);
+  }
+  
+  export async function createUserHandler(req, res) {
+    const data = req.body;
+    const newUser = await createUser(data);
+    res.status(201).json(newUser);
+  }
+  
+
+  export async function updateUserHandler(req, res) {
+    const id = parseInt(req.params.id);
+    const updates = req.body;
+    const updatedUser = await updateUser(id, updates);
+    res.status(200).json(updatedUser);
+  }
+  
+
+  export async function deleteUserHandler(req, res) {
+    const id = parseInt(req.params.id);
+    await deleteUser(id);
+    res.status(204).send();
+  }
+  
