e9050e438cd8fb65b70871e8a80378276807a0bc
Aarya Patel <aaryapatel9900@gmail.com>
2025-11-17 23:11:51 -0500

    Add authorController with CRUD handlers

---
+++ src/controllers/authorController.js
@@ -0,0 +1,51 @@
+import {
+    getAllAuthors,
+    createAuthor,
+    updateAuthor,
+    deleteAuthor,
+  } from '../services/authorService.js';
+
+  export async function getAllAuthorsHandler(req, res) {
+
+    const {
+      authorId,
+      search,
+      sortBy = 'id',  
+      sortOrder = 'desc',     
+      limit = 10,              
+      offset = 0               
+    } = req.query;
+  
+    const filter = {};
+    if (authorId) filter.authorId = authorId;
+    if (search) filter.search = search;
+    filter.sortBy = sortBy;
+    filter.sortOrder = sortOrder;
+    filter.limit = parseInt(limit);
+    filter.offset = parseInt(offset);
+  
+    let result = await getAllAuthors(filter);
+    res.status(200).json(result);
+  }
+  
+  export async function createAuthorHandler(req, res) {
+    let data = req.body; 
+    let newAuthor = await createAuthor(data);
+    res.status(201).json(newAuthor);
+  }
+  
+  
+  export async function updateAuthorHandler(req, res) {
+    let id = parseInt(req.params.id);
+    let updates = req.body;
+    const updatedAuthor = await updateAuthor(id, updates);
+    res.status(200).json(updatedAuthor);
+  }
+  
+  
+  export async function deleteAuthorHandler(req, res) {
+    let id = parseInt(req.params.id);
+    await deleteAuthor(id);
+    res.status(204).send();
+  }
+  
