582341a39bdacdbcfc47291f072661765a686907
Lori Cabrie <lcabrie@charlotte.edu>
2025-11-24 18:30:37 -0500

    changed seed file import

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,4 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
-
-const prisma = new PrismaClient();
+import prisma from '../src/config/db.js';
 
 async function main() {
   console.log("Seeding database...");
